c14ecd16602a1b4b66d0c29cab69a04b1e8b3537
Alberto <acortazar@centrosanluis.com>
2026-04-22 19:26:31 +0200

    Add files via upload

--- spotyfake/scripts/script.js
+++ spotyfake/scripts/script.js
@@ -5,6 +5,8 @@ let duracionCanciones = ['2:07', '4:33', '4:26', '4:52', '4:05', '5:08', '5:21',
 let rutaCanciones = ['audio/El Reno Renardo - 01 - Tipica Intro.mp3', 'audio/El Reno Renardo - 02 - Game Over.mp3','audio/El Reno Renardo - 03 - No Hay Huevos.mp3', 'audio/El Reno Renardo - 04 - Majestad Cuidado Con La Gravedad.mp3', 'audio/El Reno Renardo - 05 - Dios Del Balompie.mp3', 'audio/El Reno Renardo - 06 - Americano.mp3','audio/El Reno Renardo - 07 - Violenta Revolucion.mp3','audio/El Reno Renardo - 08 - Festival.mp3','audio/El Reno Renardo - 09 - Leia.mp3','audio/El Reno Renardo - 10 - Fibergran.mp3','audio/El Reno Renardo - 11 - Orcos De Mordor.mp3','audio/El Reno Renardo - 12 - Hipihopo.mp3','audio/El Reno Renardo - 13 - Preludio Al Dolor.mp3','audio/El Reno Renardo - 14 - Que Dolor.mp3','audio/El Reno Renardo - 15 - Te Das Queen.mp3','audio/El Reno Renardo - 16 - Mongomix.mp3'];
 
 const trackList = document.getElementById('trackList');
+let audioPlayer = document.getElementById('audio-player');
+let titulo = document.getElementById('current-title');
 
 canciones.forEach((cancion, i) => {
 
@@ -18,13 +20,28 @@ canciones.forEach((cancion, i) => {
         <td class="duration">${duracionCanciones[i]}</td>
     </tr>`*/
 
-    let fila = document.createElement('tr');
+    /*let fila = document.createElement('tr');
     fila.className = 'song-row';
 
     fila.addEventListener('click', function(event){
         let audioPlayer = document.getElementById('audio-player');
-        let numCancion = parseInt(event.target.parentNode.firstChild.innerText)-1;
+        //let numCancion = parseInt(event.target.parentNode.firstChild.innerText)-1;
+        let numCancion = parseInt(event.currentTarget.firstChild.innerText)-1;
         audioPlayer.src = rutaCanciones[numCancion];
+        audioPlayer.play();
+    });*/
+
+    let fila = document.createElement('tr');
+    fila.className = 'song-row';
+    fila.dataset.ruta = rutaCanciones[i];
+
+    fila.addEventListener('click', function(event) {
+        let ruta = event.currentTarget.dataset.ruta;
+
+        titulo.innerText = cancion;
+        titulo.dataset.idCancion = i;
+        document.getElementById('play-pause').innerHTML = '⏸';
+        audioPlayer.src = ruta;
         audioPlayer.play();
     });
 
@@ -57,6 +74,73 @@ canciones.forEach((cancion, i) => {
 
 });
 
+document.getElementById('volume-control').addEventListener('input', function(){
+    audioPlayer.volume = this.value/100;
+    let emojivolumen = document.getElementById('emojiVolumen');
+    if(this.value == 0){
+        emojivolumen.innerText = '🔈';
+    }else{
+        emojivolumen.innerText = '🔊';
+    }
+});
+
+document.getElementById('play-pause').addEventListener('click', function(){
+    if(this.innerText == '⏸'){
+        this.innerText = '▶';
+        audioPlayer.pause();
+    }else{
+        this.innerText = '⏸';
+
+        if(audioPlayer.src != ''){
+            audioPlayer.play();
+        }else{
+            audioPlayer.src = rutaCanciones[0];
+            titulo.innerText = canciones[0];
+            titulo.dataset.idCancion = 0;
+    
+            audioPlayer.play();
+        }
+
+
+    }
+});
+
+document.getElementById('prev').addEventListener('click', function(){
+    idCancion = parseInt(titulo.dataset.idCancion) - 1;
+
+    if(idCancion < 0){
+        audioPlayer.src = rutaCanciones[canciones.length-1];
+        titulo.innerText = canciones[canciones.length-1];
+        titulo.dataset.idCancion = canciones.length-1;
+
+        audioPlayer.play();
+    }else{
+        audioPlayer.src = rutaCanciones[idCancion];
+        titulo.innerText = canciones[idCancion];
+        titulo.dataset.idCancion = idCancion;
+
+        audioPlayer.play();
+    }
+
+});
+
+document.getElementById('next').addEventListener('click', function(){
+    idCancion = parseInt(titulo.dataset.idCancion) + 1;
+
+    if(idCancion == canciones.length){
+        audioPlayer.src = rutaCanciones[0];
+        titulo.innerText = canciones[0];
+        titulo.dataset.idCancion = 0;
+
+        audioPlayer.play();  
+    }else{
+        audioPlayer.src = rutaCanciones[idCancion];
+        titulo.innerText = canciones[idCancion];
+        titulo.dataset.idCancion = idCancion;
+
+        audioPlayer.play();       
+    }
+});
 
 /*canciones.forEach(function(cancion,i){
 
